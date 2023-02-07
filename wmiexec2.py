2f1612d4404c4e298d824a4542a694026e39f7ea
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2023-02-07 17:44:00 -0500

    Update wmiexec2.py

--- wmiexec2.py
+++ wmiexec2.py
@@ -491,7 +491,7 @@ class RemoteShell(cmd.Cmd):
             self.__pwd = self.__outputBuffer.strip('\r\n')
             self.prompt = (self.__pwd + '>')
             if self.__shell_type == 'powershell':
-                self.prompt = 'PS ' + self.prompt + ' '
+                self.prompt = ' ' + 'PS ' + self.prompt + ' '
             self.__outputBuffer = ''
 
     def default(self, line):
