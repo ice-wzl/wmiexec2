e1c866facb6d5f5f0b4538c1252f6a1b03a13d48
D3favltx90 <z.young@infosploit.com>
2023-04-03 22:19:41 -0400

    Broke out classes into seperate libraries.

---
+++ src/wmiexec2.0/wmic.py
@@ -0,0 +1,95 @@
+"""
+
+"""
+
+import logging
+import sys
+import uuid
+
+from impacket.dcerpc.v5.dcomrt import DCOMConnection
+from impacket.dcerpc.v5.dcom import wmi
+from impacket.dcerpc.v5.dtypes import NULL
+from impacket.smbconnection import SMBConnection, SMB_DIALECT, SMB2_DIALECT_002, SMB2_DIALECT_21
+
+# library within package
+from remoteshell import remoteshell
+
+OUTPUT_FILENAME = f"{{{uuid.uuid4()}}}".lower()
+
+
+class wmic:
+    """_summary_
+    """
+    def __init__(self, command='', username='', 
+                 password='', domain='', hashes=None, 
+                 aesKey=None, share=None,
+                 noOutput=False, doKerberos=False, 
+                 kdcHost=None, shell_type=None):
+        
+        self.__command = command
+        self.__username = username
+        self.__password = password
+        self.__domain = domain
+        self.__lmhash = ''
+        self.__nthash = ''
+        self.__aesKey = aesKey
+        self.__share = share
+        self.__noOutput = noOutput
+        self.__doKerberos = doKerberos
+        self.__kdcHost = kdcHost
+        self.__shell_type = shell_type
+        self.shell = None
+        
+        if hashes is not None:
+            self.__lmhash, self.__nthash = hashes.split(':')
+
+    def run(self, addr, silentCommand=False):
+        if self.__noOutput is False and silentCommand is False:
+            smbConnection = SMBConnection(addr, addr)
+            if self.__doKerberos is False:
+                smbConnection.login(self.__username, self.__password, self.__domain, self.__lmhash, self.__nthash)
+            else:
+                smbConnection.kerberosLogin(self.__username, self.__password, self.__domain, self.__lmhash,
+                                            self.__nthash, self.__aesKey, kdcHost=self.__kdcHost)
+
+            dialect = smbConnection.getDialect()
+            if dialect == SMB_DIALECT:
+                logging.info("SMBv1 dialect used")
+            elif dialect == SMB2_DIALECT_002:
+                logging.info("SMBv2.0 dialect used")
+            elif dialect == SMB2_DIALECT_21:
+                logging.info("SMBv2.1 dialect used")
+            else:
+                logging.info("SMBv3.0 dialect used")
+        else:
+            smbConnection = None
+
+        dcom = DCOMConnection(addr, self.__username, self.__password, self.__domain, self.__lmhash, self.__nthash,
+                              self.__aesKey, oxidResolver=True, doKerberos=self.__doKerberos, kdcHost=self.__kdcHost)
+        try:
+            iInterface = dcom.CoCreateInstanceEx(wmi.CLSID_WbemLevel1Login, wmi.IID_IWbemLevel1Login)
+            iWbemLevel1Login = wmi.IWbemLevel1Login(iInterface)
+            iWbemServices = iWbemLevel1Login.NTLMLogin('//./root/cimv2', NULL, NULL)
+            iWbemLevel1Login.RemRelease()
+
+            win32Process, _ = iWbemServices.GetObject('Win32_Process')
+
+            self.shell = remoteshell(self.__share, OUTPUT_FILENAME, win32Process, smbConnection, self.__shell_type, silentCommand)
+            if self.__command != ' ':
+                self.shell.onecmd(self.__command)
+            else:
+                self.shell.cmdloop()
+        except  (Exception, KeyboardInterrupt) as e:
+            if logging.getLogger().level == logging.DEBUG:
+                import traceback
+                traceback.print_exc()
+            logging.error(str(e))
+            if smbConnection is not None:
+                smbConnection.logoff()
+            dcom.disconnect()
+            sys.stdout.flush()
+            sys.exit(1)
+
+        if smbConnection is not None:
+            smbConnection.logoff()
+        dcom.disconnect()
