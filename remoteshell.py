3dbe14804af0226bebc437ee3de6ef2838c4d93c
ice-wzl <75596877+ice-wzl@users.noreply.github.com>
2023-12-26 15:00:38 -0500

    add defender location

--- remoteshell.py
+++ remoteshell.py
@@ -122,19 +122,20 @@ class RemoteShell(cmd.Cmd):
  --------------------------------------------------------------------------------------------
  + Basic Module                                                                            +  
  --------------------------------------------------------------------------------------------
-  CRTL+L                     - clear screen
-  sysinfo                    - see basic information about the host
-  lcd {path}                 - changes the current local directory to {path}
-  exit                       - terminates the server process (and this session)
+  CRTL+L                      - clear screen
+  sysinfo                     - see basic information about the host
+  lcd {path}                  - changes the current local directory to {path}
+  exit                        - terminates the server process (and this session)
   lput {src_file, dst_path}   - uploads a local file to the dst_path (dst_path = default current dir)
   lget {file}                 - downloads pathname to the current local dir
-  ! {cmd}                    - executes a local shell cmd
-  cat                        - view file contents
-  ls                         - you should know this, will show hidden files 
+  ! {cmd}                     - executes a local shell cmd
+  cat                         - view file contents
+  ls                          - you should know this, will show hidden files 
  --------------------------------------------------------------------------------------------
  + Process Accounting                                                                      +
  --------------------------------------------------------------------------------------------
   av                        - looks for Anti-Virus solutions running in the process list
+  defender                  - query Microsoft Defender status
   vmcheck                   - attempts to detect if we are running in a vm
   securitytools             - looks for security researcher tools in the process list 
  --------------------------------------------------------------------------------------------
@@ -353,6 +354,47 @@ class RemoteShell(cmd.Cmd):
             self.format_print_buff()
         except Exception as e:
             print("[!] Something went wrong, see below for error:\n", logging.critical(str(e)))
+
+    def do_defender(self, s):
+        try:
+            logging.info('Defender Install Location')
+            self.execute_remote('reg query "HKLM\Software\Microsoft\Windows Defender" /F InstallLocation | findstr /i InstallLocation')
+            self.format_print_buff()
+
+            self.execute_remote('reg query "HKLM\Software\Microsoft\Windows Defender" /F IsServiceRunning')
+            if "0x1" in self.__outputBuffer:
+                logging.info('Defender Service is Running')
+            elif "0x0" in self.__outputBuffer:
+                logging.info('Defender Service is not Running')
+            self.__outputBuffer = ''
+
+            logging.info('Defender Process Exclusions')
+            self.execute_remote('reg query "HKLM\Software\Microsoft\Windows Defender\Exclusions\Processes"')
+            if len(self.__outputBuffer.strip('\r\n')) == 0:
+                print('\tNo Process Exclusions')
+            logging.info('Defender Path Exclusions')
+            self.execute_remote('reg query "HKLM\Software\Microsoft\Windows Defender\Exclusions\Paths" | findstr /i REG_DWORD')
+            self.format_print_buff()
+
+            self.execute_remote('reg query "HKLM\Software\Microsoft\Windows Defender\Real-Time Protection" /F DisableRealtimeMonitoring')
+            if "0x0" in self.__outputBuffer:
+                logging.info('Real Time Protection is Enabled')
+            elif "0x1" in self.__outputBuffer:
+                logging.info('Real Time Protection is Disabled')
+            self.__outputBuffer = ''
+
+            self.execute_remote('reg query "HKLM\SOFTWARE\Microsoft\Windows Defender\Features" /e /F TamperProtection')
+            if "0x0" or "0x4" in self.__outputBuffer:
+                logging.info('Tamper Protection is Disabled')
+            elif "0x5" in self.__outputBuffer:
+                logging.info('Tamper Protection is Enabled')
+            else:
+                logging.info('Unknown value set for Tamper Protection')
+                self.format_print_buff()
+        except Exception as e:
+            print("[!] Something went wrong, see below for error:\n", logging.critical(str(e)))
+
+
 
     # fix this output
     def do_tokens(self, s):
